8f848ef044701dd996e695c02df7f53ccf461606
Alex Maystrenko <alexeytech@gmail.com>
2021-02-11 14:55:45 +0100

    fix import deadlock in mytest-ptvsd
    importing a module which spawns threads during import
    is a bad idea in python 2

--- mytest-ptvsd.py
+++ mytest-ptvsd.py
@@ -1,11 +1,13 @@
-from __future__ import print_function
 import ptvsd
 
 if __name__ == "__main__":
-    print("mytest-ptvsd: Enable debugger")
+    print "mytest-ptvsd: Enable debugger"
     ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
-    print("mytest-ptvsd: Waiting debugger...")
+    print "mytest-ptvsd: Waiting debugger..."
     ptvsd.wait_for_attach()
-    print("mytest-ptvsd: Debugger connected")
-    import mytest  # start enigma2
- 
+    print "mytest-ptvsd: Debugger connected"
+
+    # start enigma2
+    # use execfile instead of import, because of import lock:
+    # https://blog.sqreen.com/freeze-python-str-encode-threads/
+    execfile('/usr/lib/enigma2/python/mytest.py')
